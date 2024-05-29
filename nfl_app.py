04f284e86d584ff364741c90406cb736b713027e
Chris Fenton <chris.fenton@gusto.com>
2024-05-29 07:11:48 -0400

    first commit

--- nfl_app.py
+++ nfl_app.py
@@ -3,6 +3,6 @@ import pandas as pd
 
 st.write("First NFL App")
 st.write(pd.DataFrame({
-    'first column': [1, 2, 3, 4,6],
-    'second column': [10, 20, 30, 40,60]
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
 }))
