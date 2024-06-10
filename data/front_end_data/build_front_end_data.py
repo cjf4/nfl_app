ab60d0439c7fa143ade5054f034d436618aa691b
Chris Fenton <chris.fenton@gusto.com>
2024-06-09 22:01:14 -0400

    qb page changes

--- data/front_end_data/build_front_end_data.py
+++ data/front_end_data/build_front_end_data.py
@@ -7,62 +7,18 @@ abs_path = os.path.abspath(relative_path)
 con = duckdb.connect(database = abs_path
                      , read_only = True)
 
-first_down_query = """
-with cte as (
-	SELECT 
-	pass == 1 AS pass_play
-	, season
-	, week
-	, posteam as team
-	, count(posteam) AS plays
-	, mean(epa) AS avg_epa
-	, plays * avg_epa AS total_epa
-	
-FROM MAIN.PBP_DATA 
-WHERE 1=1
-	AND down = 1
-	AND ydstogo = 10
-	AND half_seconds_remaining > 120
-	AND ((wp > 0.1) AND (wp < .9))
-	AND NOT (play_type = 'no_play')
-GROUP BY 1,2,3,4
-
-
-)
-
-
-
-, agg as (
-select 
-  team
-  , week
-  , season
-  , sum(case when pass_play then plays else 0 end) as pass_plays
-  , sum(plays) as total_plays
-  , total_plays - pass_plays as run_plays
-  , pass_plays / total_plays as first_down_pass_rate
-  , sum(total_epa) as total_epa
-  , sum(case when pass_play then total_epa else 0 end) as pass_play_epa
-  , sum(case when not pass_play then total_epa else 0 end) as run_play_epa
-FROM cte
-where 1=1
-
-
-group by 1,2,3
-order by 1
-)
-
-select * 
-, total_epa / total_plays as first_down_epa
-, run_play_epa / run_plays as run_play_epa
-, pass_play_epa / pass_plays as pass_play_epa
-from agg
-order by season desc, team, week
-
-"""
+with open('queries/first_down_query.sql') as f:
+    first_down_query = f.read()
 
 df = con.execute(first_down_query).df()
 
+
 df.to_parquet('first_down.parquet')
+
+with open('queries/qb_epa.sql') as f:
+    qb_epa_query = f.read()
+
+df = con.execute(qb_epa_query).df()
+df.to_parquet('qb_epa.parquet')
 
 con.close()
--- nfl_app.py
+++ nfl_app.py
@@ -1,22 +1,6 @@
 import streamlit as st
-import pandas as pd
-import duckdb
 
-df = pd.read_parquet('data/front_end_data/first_down.parquet')
-
-
-st.write("Chris Fenton's NFL Analysis")
-
-
-team_selection = st.selectbox(
-    "Team",
-    ("ARI", "BUF", "GB"))
-
-season_selection = st.selectbox(
-    "Season",
-    (2023, 2022, 2021))
-
-
-st.write(df[(df.team==team_selection)
-            & (df.season==season_selection)
-            ])
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
